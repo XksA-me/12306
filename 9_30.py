c13ce938df3701e208f8f05feff9e1b2c00f590a
极简XksA <43670614+XksA-me@users.noreply.github.com>
2018-10-12 20:04:38 +0800

    查询车票json数据分析
    查询车票json数据分析

---
+++ 9_30.py
@@ -0,0 +1,15 @@
+# import os
+# file_path = os.path.abspath('image')
+# print(file_path)
+
+{
+	"data":
+		{
+			"flag":"1",
+			"map":
+				{"BXP":"北京西","HSN":"黄石","KSN":"黄石北"},
+			"result":
+				["dlmzoObh36YwI59YQTnX2xowsK5TkUNR5zjtgnVgZzGMSQzrXAgiRvUyX5%2BJogk70DD7VXfy3xSZ%0AQ1em3Sx8jlf0ZlBRshUo2FP8bhbL28klNvRb%2Bt1IlWRLvR%2FDseCZEFczu0PUufGe7KpQjD9NzfWD%0ANFLq8AXSSB7H%2FCUXFO8gNB5QTcUDqm8X%2B%2B6%2BibG4gAuneQkkKcPVcp%2BjNQQT4V8fFZmt8SaXe%2F9b%0A%2FbySWMSKH4qh7p8v%2F6%2FqTn95UqHp|预订|5u0000G49206|G492|NXG|BXP|KSN|BXP|16:45|22:42|05:57|Y|1bixVcjhrsv0uiJPZICSTjILsi3XwakHdd1YFwhAch5eHMzd|20181012|3|G1|03|12|1|0|||||||||||有|有|有||O0M090|OM9|0","aWDNjIXgPkymfvp0QiwAG8YgTo3Y7siWqhYR4uUmQPucHur12t9EE9S7%2B9zxXZ8IE0D7rdQuxV3n%0AY9Md%2BKxICrLz21wNybs3oY2luIUF1FHzkF0c1QWY6jzol7%2Fq2qMcJPG7s8WuV%2F8FmmqDd0UN%2Fsth%0AglpJcE0YkJFtuweX4LRu0hUDvWW%2BKrXljy7pSBdP7UCOFua2YvLnWaaZj0O430vPs6Q%2BoBijzSsM%0Ad5LS%2BaEQRF0tntk4sXA1WG0IMJngDCdJIonqI1c%3D|预订|570000T1680H|T168|NCG|BXP|HSN|BXP|22:02|13:05|15:03|Y|nOWlbmg4k%2Fer2sZo1byUm33oAsRW5GizDui97on0yzmdmy5qUS5BSflwJ0U%3D|20181012|3|G1|04|18|0|0||||无|||有||无|无|||||10401030|1413|0"]},
+	"httpstatus":200,
+	"messages":"",
+	"status":True}
